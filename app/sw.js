23d37788bc488995a2f2e8577cc942814626409b
Cristopher Sierra <csierra@PRODALAM.GBKCL.LOCAL>
2016-11-10 16:26:03 -0300

    initial commit

--- app/sw.js
+++ app/sw.js
@@ -24,12 +24,21 @@ self.addEventListener('push', function(event) {
   console.log('[Service Worker] Push Received.');
   console.log(`[Service Worker] Push had this data: "${event.data.text()}"`);
 
+
+
+
+
   const title = 'Push Codelab';
   const options = {
-    body: 'Yay it works.',
-    icon: 'images/icon.png',
-    badge: 'images/badge.png'
-  };
+  "body": "Did you make a $1,000,000 purchase at Dr. Evil...",
+  "icon": "images/ccard.png",
+  "vibrate": [200, 100, 200, 100, 200, 100, 400],
+  "tag": "request",
+  "actions": [
+    { "action": "yes", "title": "Yes", "icon": "images/yes.png" },
+    { "action": "no", "title": "No", "icon": "images/no.png" }
+  ]
+}
 
   event.waitUntil(self.registration.showNotification(title, options));
 });
