c89315da8f5ed1312f9e1f812df65435cde1c068
chrissierra <christopher.sierra@usach.cl>
2016-11-26 17:30:11 -0300

    Create sw1.js

---
+++ sw1.js
@@ -0,0 +1,44 @@
+var CACHE_NAME = 'gih-cache1';
+var CACHED_URLS = [
+  'indexoffline.html',
+     "jquery-1.3.2.min.js",
+        "index.html",
+        "indexedDB.js",
+        "peo.html",
+  'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css'
+  
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(CACHED_URLS);
+    })
+  );
+
+});
+
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    fetch(event.request).catch(function() {
+      return caches.match(event.request).then(function(response) {
+        return response || caches.match('indexoffline.html');
+      });
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.match(event.request).then(function(cachedResponse) {
+        var fetchPromise = fetch(event.request).then(function(networkResponse) {
+          cache.put(event.request, networkResponse.clone());
+          return networkResponse;
+        })
+        return cachedResponse || fetchPromise;
+      })
+    })
+  );
+});
